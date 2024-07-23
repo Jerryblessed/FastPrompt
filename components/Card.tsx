8e6b894edc10f7008c8111eeb62d76e5c21268fe
Jeremiah Ope <106873857+Jerryblessed@users.noreply.github.com>
2024-07-23 10:34:17 +0100

    Update Card.tsx

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,3 +1,4 @@
+// components/Card.tsx
 import Image from "next/image";
 import Link from "next/link";
 import { Post } from "@/type";
@@ -6,18 +7,15 @@ function Card({ post }: { post: Post }) {
   return (
     <div className="max-w-full m-2 sm:m-0 bg-gray-100 shadow-sm dark:bg-gray-800">
       {/* <Image className="rounded-lg p-3" width={1000} height={324} src={post.image} alt="demo image" /> */}
-
       <div className="p-3">
         <div className="flex mb-3">
           <p className="text-sm text-gray-500 dark:text-gray-400">Mar 10, 2023</p>
           <p className="text-sm text-gray-500 dark:text-gray-400 mx-1">,</p>
           <p className="text-sm text-gray-500 dark:text-gray-400">5 min read</p>
         </div>
-
         <Link className="text-lg" href={`/read/${post.title.toLowerCase().trim().split(" ").join("-")}`}>
           {post.title}
         </Link>
-
         <div className="rounded-lg p-3" style={{ width: 1000, height: 324 }}>
           <iframe
             src={`https://player.thetavideoapi.com/video/${post.url}?autoplay=0`}
