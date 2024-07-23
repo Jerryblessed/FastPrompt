4a6a2d008db11d9b483398703f9254ecf684965b
Jeremiah Ope <106873857+Jerryblessed@users.noreply.github.com>
2024-07-23 13:31:59 +0100

    Update Card.tsx

--- components/Card.tsx
+++ components/Card.tsx
@@ -14,9 +14,9 @@ function Card({ item }: { item: Posts }) {
           <p className="text-sm text-gray-500 dark:text-gray-400">5 min read</p>
         </div>
 
-        <Link className="text-lg" href={`/read/${item.title.toLowerCase().trim().split(" ").join("-")}`}>
+{/*         <Link className="text-lg" href={`/read/${item.title.toLowerCase().trim().split(" ").join("-")}`}>
           {item.title}
-        </Link>
+        </Link> */}
 
         <div className="rounded-lg p-3" style={{ width: 1000, height: 324 }}>
           <iframe
@@ -34,36 +34,3 @@ function Card({ item }: { item: Posts }) {
 
 export default Card;
 
-
-// // components/Card.tsx
-// import Image from "next/image";
-// import Link from "next/link";
-// import { Posts } from "@/type"; // Ensure this matches the correct path and export
-
-// function Card({ post }: { post: Post }) {
-//   return (
-//     <div className="max-w-full m-2 sm:m-0 bg-gray-100 shadow-sm dark:bg-gray-800">
-//       {/* <Image className="rounded-lg p-3" width={1000} height={324} src={post.image} alt="demo image" /> */}
-//       <div className="p-3">
-//         <div className="flex mb-3">
-//           <p className="text-sm text-gray-500 dark:text-gray-400">Mar 10, 2023</p>
-//           <p className="text-sm text-gray-500 dark:text-gray-400 mx-1">,</p>
-//           <p className="text-sm text-gray-500 dark:text-gray-400">5 min read</p>
-//         </div>
-//         <Link className="text-lg" href={`/read/${post.title.toLowerCase().trim().split(" ").join("-")}`}>
-//           {post.title}
-//         </Link>
-//         <div className="rounded-lg p-3" style={{ width: 1000, height: 324 }}>
-//           <iframe
-//             src={`https://player.thetavideoapi.com/video/${post.url}?autoplay=0`}
-//             width="100%"
-//             height="100%"
-//             allowFullScreen
-//           />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Card;
